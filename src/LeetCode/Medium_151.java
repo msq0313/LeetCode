1915fe077a3f7f3f3b4c3fe6fc1274fca24d461b
msq0313 <1900022750@pku.edu.cn>
2020-04-11 01:36:38 +0800

    0411

--- src/LeetCode/Medium_151.java
+++ src/LeetCode/Medium_151.java
@@ -1,8 +1,6 @@
 package LeetCode;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 151. 翻转字符串里的单词
@@ -34,6 +32,11 @@ import java.util.List;
  * 请选用 C 语言的用户尝试使用 O(1) 额外空间复杂度的原地解法。
  */
 public class Medium_151 {
+    /**
+     * 使用系统自带API，trim，reverse，spilt，正则表达式
+     * @param s
+     * @return
+     */
     public String reverseWords(String s) {
         s = s.trim();
         // 正则匹配连续的空白字符作为分隔符
@@ -41,6 +44,100 @@ public class Medium_151 {
         Collections.reverse(wordList);
         return String.join(" ", wordList);
     }
+    //自己实现函数
+    public StringBuilder trimSpaces(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') ++left;
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') --right;
+
+        // 将字符串间多余的空白字符去除
+        StringBuilder sb = new StringBuilder();
+        while (left <= right) {
+            char c = s.charAt(left);
+
+            if (c != ' ') sb.append(c);
+            else if (sb.charAt(sb.length() - 1) != ' ') sb.append(c);
+
+            ++left;
+        }
+        return sb;
+    }
+
+    public void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char tmp = sb.charAt(left);
+            sb.setCharAt(left++, sb.charAt(right));
+            sb.setCharAt(right--, tmp);
+        }
+    }
+
+    public void reverseEachWord(StringBuilder sb) {
+        int n = sb.length();
+        int start = 0, end = 0;
+
+        while (start < n) {
+            // 循环至单词的末尾
+            while (end < n && sb.charAt(end) != ' ') ++end;
+            // 翻转单词
+            reverse(sb, start, end - 1);
+            // 更新start，去找下一个单词
+            start = end + 1;
+            ++end;
+        }
+    }
+
+    /**
+     * 自己实现相应方法
+     * @param s
+     * @return
+     */
+    public String reverseWords2(String s) {
+        StringBuilder sb = trimSpaces(s);
+
+        // 翻转字符串
+        reverse(sb, 0, sb.length() - 1);
+
+        // 翻转每个单词
+        reverseEachWord(sb);
+
+        return sb.toString();
+    }
+
+    /**
+     * 双端队列
+     * @param s
+     * @return
+     */
+    public String reverseWords3(String s) {
+        int left = 0, right = s.length() - 1;
+        // 去掉字符串开头的空白字符
+        while (left <= right && s.charAt(left) == ' ') ++left;
+
+        // 去掉字符串末尾的空白字符
+        while (left <= right && s.charAt(right) == ' ') --right;
+
+        Deque<String> d = new ArrayDeque();
+        StringBuilder word = new StringBuilder();
+
+        while (left <= right) {
+            char c = s.charAt(left);
+            if ((word.length() != 0) && (c == ' ')) {
+                // 将单词 push 到队列的头部
+                d.offerFirst(word.toString());
+                word.setLength(0);
+            } else if (c != ' ') {
+                word.append(c);
+            }
+            ++left;
+        }
+        d.offerFirst(word.toString());
+
+        return String.join(" ", d);
+    }
+
     public static void main(String[] args) {
         System.out.println(new Medium_151().reverseWords("  hello world!  "));
     }
