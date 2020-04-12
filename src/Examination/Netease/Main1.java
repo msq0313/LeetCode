155aef250b79a1bc696775f818d834f2ccb51676
msq0313 <1900022750@pku.edu.cn>
2020-04-12 23:23:34 +0800

    Netease笔试

---
+++ src/Examination/Netease/Main1.java
@@ -0,0 +1,89 @@
+package Examination.Netease;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Set;
+
+/*
+1. MxM的网格，某些网格上有加成（一个正整数），其余网格值为零，你在X,Y的位置，手拿一把长为L的刀，
+可以朝任意方向砍一刀，如果砍到加成，则砍刀变长对应数值。你可以坎任意刀，请问最终刀的长度是多少？
+
+2
+10 3
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 2 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 1 0 2 0 0 0
+0 0 0 0 0 0 0 0 0 2
+0 0 0 1 0 0 0 0 0 1
+0 0 0 1 0 0 2 0 2 0
+8 8
+10 2
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 0 0
+0 0 2 0 0 1 0 0 0 0
+0 0 0 1 2 0 2 0 0 0
+0 0 0 0 2 1 2 0 2 0
+0 0 2 0 0 0 0 0 2 0
+0 0 0 0 0 0 0 2 0 0
+0 0 0 1 0 0 0 0 0 0
+2 5
+ */
+public class Main1 {
+
+    public static int distance(int x , int y, int i, int j) {
+        return (int)Math.ceil(Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j, 2)));
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int T = input.nextInt();
+        int T2 = T;
+        int[] res = new int[T];
+        while (T > 0) {
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+            int M = input.nextInt();
+            int L = input.nextInt();
+            res[T - 1] = L;
+            int[][] map = new int[M][M];
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++) {
+                    map[i][j] = input.nextInt();
+                }
+            }
+
+            int x = input.nextInt();
+            int y = input.nextInt();
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] > 0) {
+                        int distance = distance(x, y, i, j);
+//                        if (T == 1)
+//                            System.out.println(distance);
+                        hashMap.put(distance, hashMap.getOrDefault(distance, 0) + map[i][j]);
+                    }
+                }
+            }
+
+            Set<Integer> set = hashMap.keySet();
+            Object[] arr = set.toArray();
+            Arrays.sort(arr);
+            for (Object key : arr) {
+                if (res[T - 1] >= (Integer)key)
+                    res[T - 1] += hashMap.get(key);
+//                System.out.println(hashMap.get(key));
+            }
+            T--;
+        }
+        for (int i = T2 - 1; i >= 0; i--) {
+            System.out.println(res[i]);
+        }
+    }
+}
---
+++ src/Examination/Netease/Main2.java
@@ -0,0 +1,85 @@
+package Examination.Netease;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+
+1
+3 7
+3 1
+1 1 2
+3 1
+1 2 3
+3 1
+2 1
+3 1
+*/
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        ArrayList<Integer> res = new ArrayList<>();
+        for (int t = 0; t < T; t++) {
+            ArrayList<ArrayList<Integer>> vec = new ArrayList<>(N);
+            //key为数字，value为所在集合
+            HashMap<Integer, Integer> hashMap = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                ArrayList<Integer> list = new ArrayList<>();
+                vec.add(list);
+                //vec与hashMap初始化
+                vec.get(i).add(i + 1);
+                hashMap.put(i + 1, i);
+//            System.out.println(hashMap.get(i + 1));
+//            System.out.println(vec.get(i).toString());
+            }
+
+            for (int i = 0; i < M; i++) {
+                int p = sc.nextInt();
+                if (p == 1) {
+                    int x = sc.nextInt();
+                    int y = sc.nextInt();
+                    //若x，y不属于同一集合，合并
+                    int x_vec = hashMap.get(x);
+                    int y_vec = hashMap.get(y);
+                    if (x_vec != y_vec) {
+                        for (int num : vec.get(y_vec)) {
+                            vec.get(x_vec).add(num);
+                            hashMap.put(num, x_vec);
+                        }
+                        vec.get(y_vec).clear();
+                    }
+                }
+                if (p == 2) {
+                    int x = sc.nextInt();
+                    int x_vec = hashMap.get(x);
+                    if (vec.get(x_vec).size() > 1) {
+                        for (int j = 0; j < vec.get(x_vec).size(); j++) {
+                            if (vec.get(x_vec).get(j) == x) {
+                                vec.get(x_vec).remove(j);
+                                break;
+                            }
+                        }
+                        for (int j = 0; j < N; j++) {
+                            if (vec.get(j).size() == 0) {
+                                vec.get(j).add(x);
+                                hashMap.put(x, j);
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (p == 3) {
+                    int x = sc.nextInt();
+                    res.add(vec.get(hashMap.get(x)).size());
+                }
+            }
+        }
+        for (int num : res) {
+            System.out.println(num);
+        }
+    }
+}
