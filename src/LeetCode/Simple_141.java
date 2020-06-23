4cffab968d8aaaf2da132836190bdfa53cc43c50
msq0313 <1900022750@pku.edu.cn>
2020-06-23 21:02:31 +0800

    Simple_141

---
+++ src/LeetCode/Simple_141.java
@@ -0,0 +1,44 @@
+package LeetCode;
+/*141. 环形链表
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+ */
+public class Simple_141 {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+        }
+    }
+    //快慢指针
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = head.next;
+        System.out.println(new Simple_141().hasCycle(head));
+    }
+}
