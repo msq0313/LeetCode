222f1b4c55a5f489b4649bf1c0c6f1060e549acd
msq0313 <1900022750@pku.edu.cn>
2020-06-24 19:47:43 +0800

    最长上升子序列

--- src/LeetCode/Medium_300.java
+++ src/LeetCode/Medium_300.java
@@ -19,14 +19,17 @@ import java.util.Arrays;
  */
 public class Medium_300 {
 
+    //动态规划，O(n2)
     public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
         if (nums.length == 0) {
             return 0;
         }
-        int[] dp = new int[nums.length];
         Arrays.fill(dp, 1);
         int res = 1;
-        for (int i = 1; i < dp.length; i++) {
+        //dp定义为到目前为止最长上升子序列长度
+        for (int i = 0; i < dp.length; i++) {
+            //遍历当前位置之前的dp，如当前位置值比之前的大，可构成上升子序列，长度加一，保留最长长度给当前dp
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
