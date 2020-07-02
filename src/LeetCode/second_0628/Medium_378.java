53c5ed0cec48963d448f61a75b4a7d1d2d97d663
msq0313 <1900022750@pku.edu.cn>
2020-07-02 15:19:28 +0800

    378. 有序矩阵中第K小的元素

---
+++ src/LeetCode/second_0628/Medium_378.java
@@ -0,0 +1,83 @@
+package LeetCode.second_0628;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/*
+378. 有序矩阵中第K小的元素
+给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第 k 小的元素。
+请注意，它是排序后的第 k 小元素，而不是第 k 个不同的元素。
+
+示例：
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+返回 13。
+
+提示：
+你可以假设 k 的值永远是有效的，1 ≤ k ≤ n2 。
+ */
+public class Medium_378 {
+    //排序
+//    public int kthSmallest(int[][] matrix, int k) {
+//        List<Integer> list = new ArrayList<>();
+//        for(int i = 0; i < matrix.length; i++){
+//            for(int j = 0; j < matrix[0].length; j++)
+//                list.add(matrix[i][j]);
+//
+//        }
+//        Integer[] array = new Integer[list.size()];
+//        list.toArray(array);
+//        Arrays.sort(array);
+//        int res = (int)array[k - 1];
+//        return res;
+//    }
+
+    //充分利用矩阵性质，右侧比左侧大，下比上大，利用二分搜索完成
+    public int kthSmallest(int[][] matrix, int k) {
+        int n = matrix.length;
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(matrix, mid, k, n)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public boolean check(int[][] matrix, int mid, int k, int n) {
+        int i = n - 1;
+        int j = 0;
+        int num = 0;
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] <= mid) {
+                num += i + 1;
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return num >= k;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+                {1, 5, 9},
+                {10,11,13},
+                {12,13,15}
+        };
+        int k = 8;
+
+        System.out.println(new Medium_378().kthSmallest(matrix, k));
+    }
+}
