aefbbb1d52a71334319ee0822a27c26e405c7c6c
msq0313 <1900022750@pku.edu.cn>
2020-07-25 17:13:11 +0800

    剑指 Offer 10- I. 斐波那契数列

---
+++ src/Sword/Offer10.java
@@ -0,0 +1,42 @@
+package Sword;
+/*
+剑指 Offer 10- I. 斐波那契数列
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项。斐波那契数列的定义如下：
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+
+答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+
+示例 1：
+
+输入：n = 2
+输出：1
+示例 2：
+
+输入：n = 5
+输出：5
+ */
+public class Offer10 {
+    public int fib(int n) {
+        int[] res = new int[]{0, 1};
+        if (n < 2) {
+            return res[n];
+        }
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        for (int i = 2; i <= n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 45;
+        System.out.println(new Offer10().fib(n));
+    }
+}
