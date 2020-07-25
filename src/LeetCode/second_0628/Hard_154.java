1da22442048180b557bd41c0a812fc1785a31750
msq0313 <1900022750@pku.edu.cn>
2020-07-25 22:50:51 +0800

    154. 寻找旋转排序数组中的最小值 II

--- src/LeetCode/second_0628/Hard_154.java
+++ src/LeetCode/second_0628/Hard_154.java
@@ -1,5 +1,44 @@
 package LeetCode.second_0628;
+/*
+154. 寻找旋转排序数组中的最小值 II
+假设按照升序排序的数组在预先未知的某个点上进行了旋转。
 
+( 例如，数组 [0,1,2,4,5,6,7] 可能变为 [4,5,6,7,0,1,2] )。
+
+请找出其中最小的元素。
+
+注意数组中可能存在重复的元素。
+
+示例 1：
+
+输入: [1,3,5]
+输出: 1
+示例 2：
+
+输入: [2,2,2,0,1]
+输出: 0
+ */
 public class Hard_154 {
+    public int findMin(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int pivot = low + (high - low) / 2;
+            if (numbers[pivot] < numbers[high]) {
+                high = pivot;
+            } else if (numbers[pivot] > numbers[high]) {
+                low = pivot + 1;
+            } else {
+                high -= 1;
+            }
+        }
+        return numbers[low];
+    }
 
+    public static void main(String[] args) {
+        int[] numbers = new int[]{
+                2, 2, 2, 0, 1
+        };
+        System.out.println(new Hard_154().findMin(numbers));
+    }
 }
