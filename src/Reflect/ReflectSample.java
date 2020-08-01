f920277a56cce6276ee1444dab444a88082d2e46
msq0313 <1900022750@pku.edu.cn>
2020-08-02 01:44:57 +0800

    reflect

---
+++ src/Reflect/ReflectSample.java
@@ -0,0 +1,26 @@
+package Reflect;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectSample {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
+        Class rc = Class.forName("Reflect.Robot");
+        Robot r = (Robot) rc.newInstance();
+        System.out.println("Class name is " + rc.getName());
+        // 调用private方法
+        Method getHello = rc.getDeclaredMethod("throwHello", String.class);
+        getHello.setAccessible(true);
+        Object str = getHello.invoke(r, "Bob");
+        r.sayHi("bob"); // 非反射
+        System.out.println("getHello result is " + str);
+        // 调用public方法
+        Method sayHi = rc.getMethod("sayHi", String.class);
+        sayHi.invoke(r, "Welcome");
+        Field name = rc.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(r, "Alice");
+        sayHi.invoke(r, "Welcome");
+    }
+}
--- src/Sword/Offer_25.java
+++ src/Sword/Offer_25.java
@@ -33,7 +33,7 @@ public class Offer_25 {
             if (p1.val <= p2.val) {
                 cur.next = new ListNode(p1.val);
                 p1 = p1.next;
-            } else if (p1.val > p2.val) {
+            } else {
                 cur.next = new ListNode(p2.val);
                 p2 = p2.next;
             }
