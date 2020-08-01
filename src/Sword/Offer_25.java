57fc4c79df9583169c4dc7612648896827222b87
msq0313 <1900022750@pku.edu.cn>
2020-08-01 23:07:07 +0800

    剑指 Offer 25. 合并两个排序的链表

---
+++ src/Sword/Offer_25.java
@@ -0,0 +1,69 @@
+package Sword;
+/*
+剑指 Offer 25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+ */
+public class Offer_25 {
+    public static class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) {
+            return null;
+        }
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode res = new ListNode(-1);
+        ListNode cur = res;
+        while (p1 != null && p2 != null) {
+            if (p1.val <= p2.val) {
+                cur.next = new ListNode(p1.val);
+                p1 = p1.next;
+            } else if (p1.val > p2.val) {
+                cur.next = new ListNode(p2.val);
+                p2 = p2.next;
+            }
+            cur = cur.next;
+        }
+        while (p1 != null) {
+            cur.next = new ListNode(p1.val);
+            p1 = p1.next;
+            cur = cur.next;
+        }
+        while (p2 != null) {
+            cur.next = new ListNode(p2.val);
+            p2 = p2.next;
+            cur = cur.next;
+        }
+        return res.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode res = new Offer_25().mergeTwoLists(l1, l2);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
--- src/Sword/Offer_42.java
+++ src/Sword/Offer_42.java
@@ -17,24 +17,51 @@ package Sword;
 -100 <= arr[i] <= 100
  */
 public class Offer_42 {
+//    public int maxSubArray(int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//        int max = Integer.MIN_VALUE;
+//        int sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            sum += nums[i];
+//            max = Math.max(max, sum);
+//            if (sum < 0) {
+//                sum = 0;
+//            }
+//        }
+//        return max;
+//    }
+
+    // dp
     public int maxSubArray(int[] nums) {
         if (nums.length == 0) {
             return 0;
         }
-        if (nums.length == 1) {
-            return nums[0];
+        int res = nums[0];
+        int[] dp = new int[nums.length];
+        //dp[i]代表以i为结尾的最大子序和
+        int max = nums[0];
+        for (int i = 1; i < dp.length; i++) {
+            max = Math.max(nums[i], max + nums[i]);
+            res = Math.max(res, max);
         }
-        int max = Integer.MIN_VALUE;
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            max = Math.max(max, sum);
-            if (sum < 0) {
-                sum = 0;
-            }
-        }
-        return max;
+        return res;
     }
+
+    // 简化dp
+//    public int maxSubArray(int[] nums) {
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//        int res = nums[0];
+//        int max = nums[0];
+//        for (int i = 1; i < nums.length; i++) {
+//            max = Math.max(nums[i], max + nums[i]);
+//            res = Math.max(res, max);
+//        }
+//        return res;
+//    }
 
     public static void main(String[] args) {
         int[] nums = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
