7606b7cc7c19f787067772c7786810d551309728
msq0313 <1900022750@pku.edu.cn>
2021-02-12 01:07:38 +0800

    119. 杨辉三角 II

---
+++ src/Array/杨辉三角/Simple_119.java
@@ -0,0 +1,46 @@
+package Array.杨辉三角;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+119. 杨辉三角 II
+给定一个非负索引 k，其中 k ≤ 33，返回杨辉三角的第 k 行。
+
+在杨辉三角中，每个数是它左上方和右上方的数的和。
+
+示例:
+
+输入: 3
+输出: [1,3,3,1]
+进阶：
+
+你可以优化你的算法到 O(k) 空间复杂度吗？
+ */
+public class Simple_119 {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<Integer>();
+        row.add(1);
+        for (int i = 1; i <= rowIndex; ++i) {
+            row.add(0);
+            for (int j = i; j > 0; --j) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+        return row;
+    }
+
+    // 递推
+    public List<Integer> getRow2(int rowIndex) {
+        List<Integer> row = new ArrayList<Integer>();
+        row.add(1);
+        for (int i = 1; i <= rowIndex; ++i) {
+            row.add((int) ((long) row.get(i - 1) * (rowIndex - i + 1) / i));
+        }
+        return row;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> res = new Simple_119().getRow(3);
+        System.out.println(res.toString());
+    }
+}
